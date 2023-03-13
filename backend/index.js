c2c44c95faf6631745c273c2dfd41da05d7a45f9
Ammara Khan <ammarak281@gmail.com>
2023-03-13 21:56:59 +0500

    Store data into db by mongoose model

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,16 @@ const connectToMongo = require('./db');
 const express = require('express')
 
 connectToMongo();
-
 const app = express()
 const port = 3000
+
+//to use get body
+app.use(express.json())
 
 //Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  // console.log(`Example app listening on http://localhost:${port}`)
+  console.log(`App listening on http://localhost:${port}`)
 }) 
