3286a9a09cf83af6465151a67cd8caa867a04fea
Ammara Khan <ammarak281@gmail.com>
2023-03-14 12:00:38 +0500

    data validation with express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017"
+const mongoURI = "mongodb://localhost:27017/iplanner?"
 
 //connect to mongo db 
 const connectToMongo = () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,28 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+const { body, validationResult } = require('express-validator');
+
 //Create a User using: POST "/api/auth/". Dosn't require authentication
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+    // name must be atleast 5 letters
+    body('name', 'Name must be alteast 5 characters').isLength({ min: 5 }),
+    // email must be an email
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be alteast 8 characters').isLength({ min: 8 }),
+    
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user)).catch(err => {console.log(err)
+    res.json({error: 'Please enter a valid value'})});
 })
 
 module.exports = router;
