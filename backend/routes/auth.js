3d60bbaf787421829918ed2946ef5b0a4671ec8a
Ammara Khan <ammarak281@gmail.com>
2023-03-15 11:54:10 +0500

    hash password with bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
+//  import packages
+const express = require('express'); //to use express
+const router = express.Router(); // to use router from express
+const User = require('../models/User'); // to import User schema from model
+const { body, validationResult } = require('express-validator'); // to validate users
+const bcrypt = require('bcryptjs'); // to use hash and salt to encrypt user password
+var jwt = require('jsonwebtoken'); // to create json tockens for secure client-server connection
 
-const { body, validationResult } = require('express-validator');
+
+// Secret key
+const JWT_secret = 'My-SeCrEt_KeY&fOr$UsEr*ToKeN';
+
 
 //Create a User using: POST "/api/auth/createuser". Dosn't require authentication
 router.post('/createuser', [
-    // minimum length of  name
-    body('name', 'Name must be alteast 5 characters').isLength({ min: 5 }),
-    // email format must be email@mail.com format
-    body('email', 'Enter a valid email').isEmail(),
-    // minimum length of password
-    body('password', 'Password must be alteast 8 characters').isLength({ min: 8 }),
+    body('name', 'Name must be alteast 5 characters').isLength({ min: 5 }), // minimum length of  name
+    body('email', 'Enter a valid email').isEmail(), // email format must be email@mail.com format
+    body('password', 'Password must be alteast 8 characters').isLength({ min: 8 }), // minimum length of password
     
 ], async (req, res)=>{
     // if errors, return bad request
@@ -26,15 +30,25 @@ router.post('/createuser', [
         if (user){
             return res.status(400).json({error: 'User with this email already exists'})
         }
+
+        // adding hash and salt to protect user password
+        const salt = await bcrypt.genSalt(10); // create salt
+        const secPass = await bcrypt.hash(req.body.password, salt); // hash password and merge with salt
+
         // else if user email doesn't exist then create user
-        else{     
-            user = await User.create({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            })
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass
+        })
+
+        const data = {
+            user:{
+                id :user.id
+            }
         }
-        res.json(user)
+        const authtoken = jwt.sign(data, JWT_secret);
+        res.json({authtoken})
     }
     // if internal errors occur, display them 
     catch (error) {
