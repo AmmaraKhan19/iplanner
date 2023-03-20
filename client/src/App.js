70143d5ca8b15b41a7a7d2336e17393b130b0ef7
Ammara Khan <ammarak281@gmail.com>
2023-03-20 19:18:31 +0500

    Navbar and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is iPlanner App</h1>
-    </div>
+  <>
+    <Router>
+      <Navbar/>
+      <Routes>
+          <Route element={<Home />} exact path="/" />
+          <Route element={<About />} exact path="/about" />
+        </Routes>
+    </Router>
+  </>
   );
 }
 
