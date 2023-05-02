655c91e3f5f785f0336e8336636d95f9d33f4336
Ammara Khan <ammarak281@gmail.com>
2023-05-02 18:55:06 +0500

    Adding About component to client side

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,17 +5,17 @@ export const About = (props) => {
   return (
     <>
       <div className="container" >
-        <h1 className="my-3">About Us</h1>
-        {/* <div className="accordion" id="accordionExample" >
+        <h1 className="my-3">About</h1>
+        <div className="accordion" id="accordionExample" >
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingOne">
               <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                <strong>Analyze Your Text</strong>
+                <strong>About the App</strong>
               </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
               <div className="accordion-body">
-                TextUtils provides you a way to analyze your text quickly and effiently. Be it word count or anything else.
+              IPlanner provides you a way to create notes and a way to store them on the cloud. Create an account for free and enjoy the app. You can create, modify and delete your notes
               </div>
             </div>
           </div>
@@ -27,23 +27,35 @@ export const About = (props) => {
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
               <div className="accordion-body">
-                TextUtils is a free application. It does not charge anything to use any and all functions that it provides.
+              IPlanner is a free application. It does not charge anything to use any and all functions that it provides. all you need is an account to enjoy all features.
               </div>
             </div>
           </div>
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingThree">
               <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                <strong>Browser Compatible</strong>
+                <strong>Safty and Protection</strong>
               </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
               <div className="accordion-body">
-                TextUtils is a web based application that works on all the browsers like Chrome, Firefox, Edge etc.TextUtils also works on Smart Phone browsers.
+              IPlanner protects all the user data. It uses tokens to protect user passwords and uses those tokens to identify users. All passwords are encrypted for better protection. 
               </div>
             </div>
           </div>
-        </div> */}
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingFour">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                <strong>Browser Compatible</strong>
+              </button>
+            </h2>
+            <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                IPlanner is a web based application that works on all the browsers like Chrome, Firefox, Edge etc.It also works on Smart Phone browsers.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
